b263d470f4ced6e81d1133572066edbd93d64663
AmirHossein <amir6838@outlook.com>
2021-10-05 21:35:32 +0330

    captcha.1.3

--- src/captcha.php
+++ src/captcha.php
@@ -2,11 +2,7 @@
 
     session_start();
 
-<<<<<<< HEAD
-    $string = '123456789asdfhjklqwertyuiopzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-=======
-    $string = '123456789asdfghjklqwertyuiopzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
->>>>>>> 3bba314b542e7bc986767730de72a291ae81f958
+    $string = '12345678998765432100asdfghjklqwertyuiopzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
 
     $_SESSION['captcha'] = '';
     
@@ -20,17 +16,12 @@
 
     imagecolorallocate($image, 255, 255, 255);
 
-<<<<<<< HEAD
-    $colorText = imagecolorallocate($image, rand(200, 255), rand(0,70), rand(0, 100));
-=======
     $colorText = imagecolorallocate($image, rand(200, 250), rand(0,70), rand(0, 100));
->>>>>>> 3bba314b542e7bc986767730de72a291ae81f958
+
 
     $font = 'Ding-DongDaddyO.ttf';
 
-    imagefttext($image, 20, rand(-10, 30), rand(0,50), rand(0,50), colorLine($image), $font, '_________');
 
-<<<<<<< HEAD
     imagefttext($image, 20, rand(-10, 30), rand(0,50), rand(0,50), colorLine($image), $font, '__________');
 
     imagefttext($image, 20, rand(-10, 30), rand(0,50), rand(0,50), colorLine($image), $font, '__________');
@@ -38,24 +29,17 @@
     imagefttext($image, 20, rand(-10, 30), rand(0,50), rand(0,50), colorLine($image), $font, '__________');
 
     imagefttext($image, 20, rand(-10, 10), rand(0, 40), rand(30, 35), $colorText, $font, $_SESSION['captcha']);
-=======
-
-    imagefttext($image, 20, rand(-10, 10), rand(0, 60), rand(30, 35), $colorText, $font, $_SESSION['captcha']);
 
     imagefttext($image, 20, rand(-10, 30), rand(0,50), rand(0,50), colorLine($image), $font, '__________');
-
-    imagefttext($image, 20, rand(-10, 30), rand(0,50), rand(0,50), colorLine($image), $font, '__________');
->>>>>>> 3bba314b542e7bc986767730de72a291ae81f958
 
 
     imagepng($image);
 
     function colorLine($image){
-<<<<<<< HEAD
         return imagecolorallocate($image, rand(50, 255), rand(50,255), rand(50, 255));
-=======
+
         return imagecolorallocate($image, rand(0, 255), rand(0, 255), rand(0, 255));
->>>>>>> 3bba314b542e7bc986767730de72a291ae81f958
+
     }
     
 ?>
