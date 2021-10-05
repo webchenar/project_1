976fbdfd7ee893c6a94944e960e3df1851a8224a
AmirHossein <amir6838@outlook.com>
2021-10-05 22:08:44 +0330

    login.captcha.1.1

--- login.php
+++ login.php
@@ -13,6 +13,13 @@ if (isset($_POST['phone']) and $_POST['password']) {
 
     $user = $data->searchLogIn('tbl_user', 'phone', $_POST['phone'], 'PASSWORD', md5($_POST['password']));
 
+    if (strcmp($_SESSION['captcha'], $_POST['code']) != 0) {
+        echo '<div class="container my-2 alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>کد امنیتی اشتباه است</strong>
+      </div>';
+        $chek = false;
+    }
+
     if (!$user) {
         echo '<div class="container my-2 alert alert-danger alert-dismissible fade show" role="alert">
         <strong>نام کاربری یا رمز عبور اشتباه است</strong>
@@ -40,19 +47,16 @@ if (isset($_POST['phone']) and $_POST['password']) {
         if (isset($_POST['check'])) {
 
             //session_start();
-            
+
             $_SESSION['phone'] = $user['phone'];
             $_SESSION['fname'] = $user['first_name'];
+        } else {
 
-        }else{
-
-            setcookie('phone', $user['phone'], time()+10800);
-            setcookie('fname', $user['first_name'], time()+10800);
-
+            setcookie('phone', $user['phone'], time() + 10800);
+            setcookie('fname', $user['first_name'], time() + 10800);
         }
 
         header('location:index.php');
-
     }
 }
 
@@ -67,7 +71,7 @@ if (isset($_POST['phone']) and $_POST['password']) {
 
         <div class="row d-flex justify-content-center ">
 
-            <form class="my-2 needs-validation col-md-6 " action="login.php" method="POST" novalidate>
+            <form class="my-2 needs-validation col-md-5 " action="login.php" method="POST" novalidate>
 
                 <div class=" col-12 my-3">
                     <label for="inputfname" class="form-label">شماره همراه:*</label>
@@ -86,12 +90,23 @@ if (isset($_POST['phone']) and $_POST['password']) {
                     </div>
                 </div>
 
+                <div class="col-12 col-md-12 my-3">
+                    <label for="inputfname" class="form-label">تصویر امنیتی:<span class="t-red">*</span></label>
+                    <img src="./src/captcha.php" class="my-4" alt="">
+                    <input type="text" name="code" id="validationCustom03 phone" class="form-control" placeholder="لطفا کد امنیتی را وارد کنید" maxlength="5" required>
+                    <div class="invalid-feedback">
+                        وارد کردن کد امنیتی اجباریست
+                    </div>
+                    <span id="spanmsg"></span>
+                </div>
+
                 <div class="mb-3 mt-3 form-check">
 
                     <input type="checkbox" name="check" value="1" class="form-check-input " id="exampleCheck1">
 
                     <label class="form-check-label d" for="exampleCheck1">مرا به خاطر بسپار</label>
                 </div>
+
 
                 <button class="btn  btn-info  text-center col-12" type="submit" id="button-addon2">
                     ورود
