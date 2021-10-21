1a5bb098e743196fa34e9b4bad2cfb28e3fcf927
Milad <91380253+Milad-MM@users.noreply.github.com>
2021-10-21 16:03:51 +0330

    last changes in admin (#17)
    * 1
    
    * clean up.
    
    * delete file
    
    * insert admin part
    
    * insert some empty file and some little changes.
    
    * create admin login page
    
    * create admin menu
    
    * insert admin page footer
    
    * admin dashboard insert footer and menu
    
    * fix body and  html end tag in footer
    
    * fix html tags for including to index.php
    
    * logout mechanism
    
    * admin dashboard.
    
    * insert logout menu
    
    * fix address
    
    * fix address again and delete nikoosabt
    
    * fix address
    
    * fix address
    
    * insert admin manager page
    
    * add admin file
    
    * delete admin file
    
    * fix manage admin
    
    * add a TODO
    
    * update admin file.
    
    * fix local addresses.
    
    * add admin page
    
    * last date time update - fixed
    
    * set default timezone.
    
    * fix update date and time in database. last date time work correctly now.
    
    * add admin back-end.
    
    * fix add admin. debug and complete the mechanism.
    
    * fix delete admin mechanism
    
    * merge amirhosein files and changes.
    
    * fix changes
    
    * fix admin page.
    
    * fix last changes.
    
    * fix delete admin.
    
    * fix manage admin. last changes.
    
    * fix update admin.
    
    * fix password changing mechanism for admin.
    
    * fix persian utf8
    
    * fix character set.
    
    * add a first admin
    
    * create Sabt_sherkat table in Database
    
    * change siteURL config
    
    * responsive admin
    
    * change admin for make all web page, responsive
    
    * fix admin front-end
    
    * add buffer start function
    
    * create a footer and change addresses.
    
    * fix menu file and make a main menu. menu have nav and sidebar.
    
    * add request page and sj page
    
    * insert sj and sahamdaran tables in database.
    
    * add and change database creation codes
    
    * fix tables
    
    * fix button
    
    * fix some space.
    
    * add last 3 field in tables.
    
    * fix tables
    
    * request sj styles.
    
    * clean front-end codes.
    
    * delete front-end and make files clean
    
    * fix 2
    
    * delete some files and fix files again
    
    * fix headers and partials
    
    * convert files to specific folder
    
    * insert header and menu in administrator files
    
    * fix admin management, add admin, directories, links, includes and etc.
    
    * fix admins
    
    * fix requests
    
    * last changes
    
    Co-authored-by: Milad-MM <Milad-MM@outlook.com>
    Co-authored-by: AmirHossein <90980342+amir6838@users.noreply.github.com>

--- admin/administrator/admin-login.php
+++ admin/administrator/admin-login.php
@@ -4,9 +4,9 @@ include "../../config/db_config.php";
 include '../partials/header.php';
 ?>
     <!--insert styles and Links here.-->
-
+    <link href="../styles/all.css" rel="stylesheet"/>
     <link href="../styles/login.css" rel="stylesheet"/>
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
           integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
           crossorigin="anonymous"/>
