d201bbe5398fc5650130011b05cee3ed8540af4e
amingrt <amin.gharaat12@gmail.com>
2022-01-03 10:53:37 +0330

    nikosabtiha

--- js/main.js
+++ js/main.js
@@ -41,3 +41,26 @@ $(window).scroll(function(){
 });
 
 
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        900:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
