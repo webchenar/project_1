5459731e465e4cc065130694f7b5658e925e4ff6
amin-gharaat <91380757+amingrt@users.noreply.github.com>
2022-01-06 04:53:09 +0330

    sabte-shekat (#36)
    * khadamat and sabte-sherkat
    
    * sabte-sherkat
    
    * sabte-sharkat

---
+++ khadamat-sabtesherkat-tazamoni.php
@@ -0,0 +1,100 @@
+
+<?php
+include_once('./header.php');
+?>
+
+
+
+
+
+
+<div class="container-fluid namaiandegiha-bg-color-fluid">
+  <div class="container">
+    <div class="row ">
+      <div class="col-12">
+
+        <div class="namaiandegiha-style my-5">
+          <div class="d-flex justify-content-center align-items-center title-moshavereh mt-3">
+            <span class="text-center text-white p-3 my-4">ثبت شرکت شرکت تضامنی </span>
+          </div>
+          <p class="font-color">
+          شرکتی است که تحت اسم مخصوصی برای امور تجارتی بین دو یا چند نفر با مسئولیت تضامنی تشکیل می‌شود و اگر دارایی شرکت برای تادیه تمام قروض شرکت کافی نباشد ٬هر یک از شرکا مسئول پرداخت تمام قروض هستند. در صورتی که نام برخی مالکان شرکت در نام شرکت آمده باشد، شرکت تضامنی محسوب می‌شود.
+          </p>
+          <p class="p-0 my-3 fs-4 d-flex align-items-center justify-content-center shadow-p">
+          مراحل مورد نیاز برای ثبت شرکت تضامنی
+          </p>
+          <p class="color-gray">
+
+          شرکت تضامنی بعد از انجام این امور قابل تاسیس می باشد:
+          </p>
+        
+      
+          <p class="font-color">
+            <i class="fas fa-caret-left"></i>
+            شرکتنامه مطابق قانون تنظیم شده باشد .
+          </p>
+          <p>
+            <i class="fas fa-caret-left"></i>
+      
+            تمام سرمایه نقدی تأدیه و سهم الشرکه غیر نقدی تماماً تقویم و تحویل شده باشد.
+            </p>
+          <p class=" font-color">
+
+            <i class="fas fa-caret-left"></i>
+            معمولا شرکت تضامنی نیازی به اساسنامه ندارد اما در صورت وجود قراری بین شرکاء در صورت نیاز در اساسنامه قید می شود زیرا در اساسنامه شرکت تضامنی امور اساسی درج می شود مواردی مانند:
+
+ نام شرکت،
+
+ موضوع شرکت،
+
+مدت فعالیت شرکت،
+
+سرمایه نقدی و یا غیر نقدی هریک از شرکاء با درج نام شرکاء،
+
+ شرایط تقسیم سود و زیان،
+
+ قوانین مربوط به فوت شرکاء و یا انحلال شرکت به هر دلیل
+          </p>
+          <p class="p-0 my-3 fs-4 d-flex align-items-center justify-content-center shadow-p">
+          مدارک لازم جهت ثبت شرکت تضامنی
+          </p>
+         
+          <p class="font-color">
+            <i class="fas fa-caret-left"></i>
+            دو برگ تقاضانامه
+          </p>
+          <p class="font-color">
+            <i class="fas fa-caret-left"></i>
+            دو برگ شرکت‌نامه
+          </p>
+          <p class="font-color">
+            <i class="fas fa-caret-left"></i>
+            دو نسخه اساسنامه
+          </p>
+          <p class="font-color">
+            <i class="fas fa-caret-left"></i>
+            دو نسخه صورتجلسه مجمع و هیات مدیره
+          </p>
+          <p class="font-color">
+            <i class="fas fa-caret-left"></i>
+            مجوز از مراجع ذیصلاح در صورت نیاز 
+          </p>
+          <p class="font-color">
+            <i class="fas fa-caret-left"></i>
+            اصل وکالتنامه ی وکیل دادگستری در صورتی که ثبت شرکت توسط وکیل صورت پذیرد
+          </p>
+    
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+
+
+
+
+
+
+<?php 
+include_once('./footer.php')
+ ?>
